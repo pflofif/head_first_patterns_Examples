4f7f79361ce45fd1cad9152c1e93c56eb1656e88
pflofif <bogdanvivchar8@gmail.com>
2022-11-01 15:51:37 +0200

    add architecture image

--- Decorator/DrinkDecorators/DrinkDecorator.cs
+++ Decorator/DrinkDecorators/DrinkDecorator.cs
@@ -6,20 +6,3 @@ public abstract class DrinkDecorator : Drink
 {
 
 }
-
-public class Mocha : DrinkDecorator
-{
-    private readonly Drink _drink;
-    private double averageTempOnMars = 123123;
-
-    public Mocha(Drink drink)
-    {
-        _drink = drink;
-        Description = $"{_drink.Description} + Mocha";
-    }
-
-    public override double Cost()
-    {
-        return 0.20 + _drink.Cost() + averageTempOnMars;
-    }
-}
