740596ca79523c664dda87f3ad70870c940956e4
pflofif <bogdanvivchar8@gmail.com>
2022-11-11 17:03:15 +0200

    ad

--- bebra/Class1.cs
+++ bebra/Class1.cs
@@ -1,7 +1,17 @@
-﻿namespace bebra
+﻿using System.Runtime.InteropServices;
+
+namespace bebra
 {
     public class Class1
     {
-
+        private int f;
+        private int a;
+        private int fd;
+        private int ff;
+        private int kf;
+        private int hf;
+        private int faf;
+        private int df;
+        private int af;
     }
 }
